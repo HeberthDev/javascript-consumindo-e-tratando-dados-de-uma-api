b3ec37f2f2f7696354197ddcb0821760793a8dd8
HeberthDev <heberthdeveloper@gmail.com>
2024-07-26 08:32:26 -0300

    Construindo a lógica de pesquisa

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ async function buscarEMostrarVideos() {
 
 buscarEMostrarVideos();
 
-const barraDePesquisa = document.querySelector('.pesquisa__input');
+const barraDePesquisa = document.querySelector('.pesquisar__input');
 
 barraDePesquisa.addEventListener('input', filtrarPesquisa);
 
@@ -43,8 +43,19 @@ function filtrarPesquisa() {
     const videos = document.querySelectorAll('.videos__item');
 
     if (barraDePesquisa.value != '') {
+        for (let video of videos) {
+            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
 
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = 'none';
+            } else {
+                video.style.display = 'block';
+            }
+        }
     } else {
-        
+        for (let video of videos) {
+            video.style.display = 'block'; //inclusão de 'for' por dar erro de não encontrar "video". "Video" é um valor que está dentro do 'if' logo não é encontrado. 
+        }
     }
 }
