bdec829162d308022cf8c459faab98b61bb7ddf7
HeberthDev <heberthdeveloper@gmail.com>
2024-07-26 07:59:38 -0300

    Async/Await

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch('http://localhost:3000/videos')
-.then(res => res.json())
-.then((videos) => 
+async function buscarEMostrarVideos() {
+    const busca = await fetch('http://localhost:3000/videos');
+    const videos = await busca.json();
+
     videos.forEach((video) => {
         containerVideos.innerHTML += `
             <li class="videos__item">
@@ -21,8 +22,7 @@ const api = fetch('http://localhost:3000/videos')
                 </div>
             </li>
         `;
-    }),
-)
-.catch((error) => {
-    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`;
-});
+    });
+}
+
+buscarEMostrarVideos();
