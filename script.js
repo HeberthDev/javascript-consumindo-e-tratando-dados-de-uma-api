736fedcb2998ff10cedfe78dc79232282544e481
HeberthDev <heberthdeveloper@gmail.com>
2024-07-26 08:42:53 -0300

    Criando a lógica de filtragem

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ async function buscarEMostrarVideos() {
     
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
             `;
@@ -72,6 +73,13 @@ function filtrarPorCategoria(filtro) {
     const videos = document.querySelectorAll('.videos__item');
 
     for (let video of videos) {
-        
+        let categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if (!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = 'none';
+        } else {
+            video.style.display = 'block';
+        }
     }
 }
